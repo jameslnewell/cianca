b532ce088a354611e5cb89ba86e845ef94cc12da
James Newell <james@digitaledgeit.com.au>
2015-10-21 07:44:37 +1100

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# cianca
+
+An isomorphic router for React.
+
+https://www.youtube.com/watch?v=nSt4J2QxQgs
+
+## Installation
+
+    npm install --save cianca
+
+## Usage
+
+    
---
+++ index.js
@@ -0,0 +1,7 @@
+import Router from './lib/router';
+import Link from './lib/components/Link';
+import Component from './lib/components/Router';
+
+export default Router;
+export {Link as Link};
+export {Component as Router};
